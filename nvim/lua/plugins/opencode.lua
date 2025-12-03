1b51383528aa7f7891564d8116e86dd8694eb638
Adam Wilczek <adam.wilczek@protonmail.com>
2025-12-03 11:24:11 +0100

    update binding opencode

--- nvim/lua/plugins/opencode.lua
+++ nvim/lua/plugins/opencode.lua
@@ -7,61 +7,121 @@ return {
       ---@module 'snacks' <- Loads `snacks.nvim` types for configuration intellisense.
       { "folke/snacks.nvim", opts = { input = {}, picker = {}, terminal = {} } },
     },
+    keys = {
+      {
+        "<C-a>",
+        function()
+          require("opencode").ask("@this: ", { submit = true })
+        end,
+        mode = { "n", "x" },
+        desc = "Ask opencode",
+      },
+      {
+        "<C-e>",
+        function()
+          require("opencode").select()
+        end,
+        mode = { "n", "x" },
+        desc = "Execute opencode action",
+      },
+      {
+        "ga",
+        function()
+          require("opencode").prompt("@this")
+        end,
+        mode = { "n", "x" },
+        desc = "Add to opencode",
+      },
+      {
+        "<C-t>",
+        function()
+          require("opencode").toggle()
+        end,
+        mode = { "n", "t" },
+        desc = "Toggle opencode",
+      },
+      {
+        "<S-C-u>",
+        function()
+          require("opencode").command("session.half.page.up")
+        end,
+        desc = "opencode half page up",
+      },
+      {
+        "<S-C-o>",
+        function()
+          require("opencode").command("session.half.page.down")
+        end,
+        desc = "opencode half page down",
+      },
+      {
+        "<leader>aa",
+        function()
+          require("opencode").ask("@this: ", { submit = true })
+        end,
+        mode = { "n", "x" },
+        desc = "Ask about this",
+      },
+      {
+        "<leader>as",
+        function()
+          require("opencode").select()
+        end,
+        mode = { "n", "x" },
+        desc = "Select prompt",
+      },
+      {
+        "<leader>au",
+        function()
+          require("opencode").prompt("@this")
+        end,
+        mode = { "n", "x" },
+        desc = "Add this",
+      },
+      {
+        "<leader>at",
+        function()
+          require("opencode").toggle()
+        end,
+        desc = "Toggle embedded",
+      },
+      {
+        "<leader>ac",
+        function()
+          require("opencode").command()
+        end,
+        desc = "Select command",
+      },
+      {
+        "<leader>an",
+        function()
+          require("opencode").command("session_new")
+        end,
+        desc = "New session",
+      },
+      {
+        "<leader>ai",
+        function()
+          require("opencode").command("session_interrupt")
+        end,
+        desc = "Interrupt session",
+      },
+      {
+        "<leader>aA",
+        function()
+          require("opencode").command("agent_cycle")
+        end,
+        desc = "Cycle selected agent",
+      },
+    },
     config = function()
       ---@type opencode.Opts
       vim.g.opencode_opts = {
-        -- Your configuration, if any � see `lua/opencode/config.lua`, or "goto definition".
+        -- Your configuration, if any  see `lua/opencode/config.lua`, or "goto definition".
       }
 
       -- Required for `opts.events.reload`.
       vim.o.autoread = true
-
-      -- Recommended/example keymaps.
-      vim.keymap.set({ "n", "x" }, "<C-a>", function()
-        require("opencode").ask("@this: ", { submit = true })
-      end, { desc = "Ask opencode" })
-      vim.keymap.set({ "n", "x" }, "<C-e>", function()
-        require("opencode").select()
-      end, { desc = "Execute opencode action" })
-      vim.keymap.set({ "n", "x" }, "ga", function()
-        require("opencode").prompt("@this")
-      end, { desc = "Add to opencode" })
-      vim.keymap.set({ "n", "t" }, "<C-t>", function()
-        require("opencode").toggle()
-      end, { desc = "Toggle opencode" })
-      vim.keymap.set("n", "<S-C-u>", function()
-        require("opencode").command("session.half.page.up")
-      end, { desc = "opencode half page up" })
-      vim.keymap.set("n", "<S-C-o>", function()
-        require("opencode").command("session.half.page.down")
-      end, { desc = "opencode half page down" })
-      -- You may want these if you stick with the opinionated "<C-a>" and "<C-x>" above � otherwise consider "<leader>a".
-      -- vim.keymap.set("n", "+", "<C-a>", { desc = "Increment", noremap = true })
-      -- vim.keymap.set("n", "-", "<C-x>", { desc = "Decrement", noremap = true })
-      vim.keymap.set({ "n", "x" }, "<leader>aa", function()
-        require("opencode").ask("@this: ", { submit = true })
-      end, { desc = "Ask about this" })
-      vim.keymap.set({ "n", "x" }, "<leader>as", function()
-        require("opencode").select()
-      end, { desc = "Select prompt" })
-      vim.keymap.set({ "n", "x" }, "<leader>au", function()
-        require("opencode").prompt("@this")
-      end, { desc = "Add this" })
-      vim.keymap.set("n", "<leader>at", function()
-        require("opencode").toggle()
-      end, { desc = "Toggle embedded" })
-      vim.keymap.set("n", "<leader>ac", function()
-        require("opencode").command()
-      end, { desc = "Select command" })
-      vim.keymap.set("n", "<leader>an", function()
-        require("opencode").command("session_new")
-      end, { desc = "New session" })
-      vim.keymap.set("n", "<leader>ai", function()
-        require("opencode").command("session_interrupt")
-      end, { desc = "Interrupt session" })
-      vim.keymap.set("n", "<leader>aA", function()
-        require("opencode").command("agent_cycle")
-      end, { desc = "Cycle selected agent" })
     end,
   },
 }
